028e005d976519880ccfdbec80737de9e71a88a3
ViniGSouza <vinizintwitchtv@gmail.com>
2024-01-23 15:05:26 -0300

    register function att

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 export const registerUser = async (name: string, email: string, password: string, phone: string) => {
   try {
     const response = await axios.post(`${process.env.API_URL}/users`, {
@@ -8,11 +7,6 @@ export const registerUser = async (name: string, email: string, password: string
       email,
       password,
       phone
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
     })
     return response.data;
   } catch (error) {
